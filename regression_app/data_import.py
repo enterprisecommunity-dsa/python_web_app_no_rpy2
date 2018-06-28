f8c7f8400c623c7c8124c6dc271dfb6b8c6c3b72
Melia <dsa@enterprisecommunity.onmicrosoft.com>
2018-06-28 10:00:48 -0400

    Branch without rpy2 to test Heroku production

--- regression_app/data_import.py
+++ regression_app/data_import.py
@@ -33,9 +33,11 @@ def import_data():
 	
 @bp.route('/view_results', methods = ('GET',))
 def view_results():
+
+	if session.get('dat'):
 		from pandas import DataFrame, read_csv
 		error = None
-		from . import linear_modeling_engine
+		#from . import linear_modeling_engine
 		from io import StringIO
 		
 		
@@ -47,43 +49,16 @@ def view_results():
 		#except ParserError:
 			#return redirect(url_for('data_import.data_import'))
 		
-		coef = linear_modeling_engine.call_r(x)
+		#coef = linear_modeling_engine.call_r(x)
 		
 		cc = []
 		
-		for i in coef:
+		for i in x.mean():
 			cc.append(i)
 		
 		return render_template('view_results.html', coef=cc)
-		
-		
-		
-def any_function_call():
-	return 0
-	
-		
-		
-def second_call_r(df):
-	import rpy2.robjects
-	#from rpy2.robjects import pandas2ri
-	from rpy2.robjects.packages import importr
-	stats = importr('stats')
-	base = importr('base')
-	'''
-	pandas2ri.activate()
-	rpy2.robjects.globalenv['dataframe'] = df
-	m = stats.lm('dataframe', data=base.as_symbol('dataframe'))
-	return m
-	'''
-	
-'''
-12,14,15,2
-0,7,4,2
-12,5,5,5
-1,2,3,4
-
-
-'''
+	else:
+		return redirect(url_for('data_import.import_data'))
 
   
 		
