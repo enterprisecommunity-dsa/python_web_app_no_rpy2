ce2eb7eac2da3b4e604c4ed96100a2c27ee37a00
Melia <dsa@enterprisecommunity.onmicrosoft.com>
2018-07-26 14:17:22 -0400

    Pushingto heroku

--- regression_app/R_calling_functions.py
+++ regression_app/R_calling_functions.py
@@ -7,7 +7,7 @@ def lm_output_printer():
 	'''
 	
 	cmd = str(os.path.join(os.getcwd(), 'bin/Rscript'))
-	cmd = cmd + str(os.getcwd())
+	cmd = cmd + ' ' +str(os.path.join(os.getcwd(), '/regression_app/csv_reader_new.R')
 	
 	# filename = str(session['current_data_abs_path'])
 	# fe = str(session['current_file_extension'])
@@ -23,7 +23,7 @@ def lm_output_printer():
 	current_data_txt = session['current_data']
 	cmd_line_text = dict_to_command_line_string(csv_to_dict(current_data_txt))
 	#cmd += ' ' + str(os.getcwd()) + '/regression_app/csv_reader_new.R ' 
-	#cmd += session['current_dependent_variable']+ ' '+ cmd_line_text
+	cmd += session['current_dependent_variable']+ ' '+ cmd_line_text
 	
 	import subprocess
 	try:
